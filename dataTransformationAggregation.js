57fbfd24578a0f6402b032a7d591b6dea82fa470
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-04-27 01:29:57 +0600

    From Simple Totals to Powerful Aggregation (reduce)

--- dataTransformationAggregation.js
+++ dataTransformationAggregation.js
@@ -228,3 +228,32 @@ const rawApiData = [
 //   );
 // };
 // console.log(range(0, 11, 2));
+
+//From Simple Totals to Powerful Aggregation (reduce)
+// const cartItems = [
+//   { id: "p-001", name: "Daraz Laptop Bag", price: 1500, quantity: 1 },
+//   { id: "p-002", name: "Walton USB-C Cable", price: 350, quantity: 2 },
+//   { id: "p-003", name: "Aarong Kurta", price: 2200, quantity: 1 },
+// ];
+// const total = cartItems.reduce(
+//   (accu, curr) => accu + curr.price * curr.quantity,
+//   0,
+// );
+
+// console.log(total);
+
+// Find best scorer
+const players = [
+  { name: "Jamal Bhuyan", score: 88 },
+  { name: "Shekh Morsalin", score: 81 },
+  { name: "Rakib Hossain", score: 95 },
+  { name: "Topu Barman", score: 91 },
+  { name: "Sohel Rana", score: 72 },
+];
+
+const bestScorer = players.reduce((bestPlayer, player) => {
+  if (bestPlayer.score > player.score) return bestPlayer;
+  return player;
+}, players[0]);
+
+console.log(bestScorer);
