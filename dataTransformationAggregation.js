9abfedf3d632844dc3ade29449cadc8a9e3b4a26
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-04-28 02:15:39 +0600

    Scenario Based Activity - Aggregating Data

--- dataTransformationAggregation.js
+++ dataTransformationAggregation.js
@@ -342,3 +342,68 @@ const rawApiData = [
 // }, {});
 
 // console.log(count);
+
+//* Grouping and Aggregating Data
+
+// Scenario: You have a flat array of sales data, and you need to group the sales by category,
+// calculating the total revenue and the number of items sold for each.
+/*
+const sales = [
+  { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
+  { category: "Books", item: "JS Basics", price: 30, quantity: 2 },
+  { category: "Electronics", item: "Mouse", price: 25, quantity: 2 },
+  { category: "Home", item: "Chair", price: 150, quantity: 1 },
+  { category: "Books", item: "React Deep Dive", price: 50, quantity: 1 },
+  { category: "Electronics", item: "Keyboard", price: 80, quantity: 1 },
+];
+
+//? Output
+// {
+//   Electronics: {
+//     totalRevenue: 1330,
+//     itemCount: 4,
+//   },
+//   Books: {
+//     totalRevenue: 110,
+//     itemCount: 3,
+//   },
+//   Home: {
+//     totalRevenue: 150,
+//     itemCount: 1,
+//   },
+// };
+
+// const output = sales.reduce((table, sale) => {
+//   const category = sale.category;
+//   if (!table[sale.category]) {
+//     table[category] = {
+//       totalRevenue: sale.price * sale.quantity,
+//       itemCount: sale.quantity,
+//     };
+//   } else {
+//     table[category].totalRevenue += sale.price * sale.quantity;
+//     table[category].itemCount += sale.quantity;
+//   }
+//   return table;
+// }, {});
+// console.log(output);
+
+const output = sales.reduce((table, sale) => {
+  const { category, price, quantity } = sale;
+  // table[category] = {
+  //   totalRevenue: 0,
+  //   itemCount: 0,
+  // };
+  if (!table[category]) {
+    table[category] = {
+      totalRevenue: price * quantity,
+      itemCount: quantity,
+    };
+  } else {
+    table[category].totalRevenue += price * quantity;
+    table[category].itemCount += quantity;
+  }
+  return table;
+}, {});
+console.log(output);
+*/
